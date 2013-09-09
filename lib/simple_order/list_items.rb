bc870c5c0a9cf59f00177b0cd541533c4e37d0f3
Felipe Seixas <seixasfelipe@gmail.com>
2013-09-09 15:41:53 -0400

    adds items to list items collection.

--- lib/simple_order/list_items.rb
+++ lib/simple_order/list_items.rb
@@ -6,8 +6,14 @@ module SimpleOrder
       @list_items = []
     end
 
-    def push(item)
-      @list_items << item
+    def push(item, qty=1)
+      pushed = false
+      self.each { |h|
+        h[:qty] += qty if h[:item] == item
+        pushed = true
+      }
+      
+      @list_items << { item: item, qty: qty } unless pushed
     end
 
     def length
@@ -18,7 +24,8 @@ module SimpleOrder
       @list_items.each(&block)
     end
 
-    alias_method :<<, :push
+    alias_method :<<  , :push
+    alias_method :add , :push
     alias_method :size, :length
 
   end
--- spec/simple_order/list_item_spec.rb
+++ spec/simple_order/list_item_spec.rb
@@ -3,15 +3,52 @@ require 'spec_helper'
 module SimpleOrder
   describe ListItems do
 
+    let(:list_items) { ListItems.new }
     let(:item) { { name: "item 1" } }
-    
-    it "should add an item" do
-      list_items = ListItems.new
-      list_items << item
-      
-      list_items.should have_exactly(1).item
-      list_items.first.should be_eql item
+
+    describe "#adding one item" do    
+      it "using push method" do
+        list_items.push item
+        validates_addition_of item, 1
+      end
+
+      it "using << method" do
+        list_items << item
+        validates_addition_of item, 1
+      end
+
+      it "using add method" do
+        list_items.add item
+        validates_addition_of item, 1
+      end
     end
 
+    describe "#adding one item twice" do
+      it "using add twice" do
+        list_items.add item
+        list_items.add item
+        validates_addition_of item, 2
+      end
+
+      it "using add once" do
+        list_items.add item, 2
+        validates_addition_of item, 2
+      end
+    end
+
+    describe "#adding one item three times" do
+      it "using add once and twice after" do
+        list_items.add item
+        list_items.add item, 2
+        validates_addition_of item, 3
+      end
+    end
+
+    private
+
+    def validates_addition_of(item, qty)
+      list_items.should have_exactly(1).item
+      list_items.first.should be_eql({ item: item, qty: qty})
+    end
   end
 end
